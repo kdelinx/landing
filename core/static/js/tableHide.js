afdee69476d84255f1831cd06e633e6448a6a640
kdelinx <kdelinx@gmail.com>
2015-06-09 12:22:56 +0600

    add new battery

---
+++ core/static/js/tableHide.js
@@ -0,0 +1,28 @@
+jQuery(function() {
+   jQuery('table.tg input').on('click', function(event) {
+        console.log('clicked');
+        var th = event.currentTarget.parentElement,
+            tr = th.parentElement,
+            index = 0,
+            child = tr.firstChild;
+
+        while((child = child.nextSibling) != null) {
+            if (child != th){
+                 index++;
+            } else {
+                break;
+            }
+        }
+
+       jQuery('table tr').each(function() {
+           var i = 0,
+               child = this.firstChild;
+
+           while (((child = child.nextSibling) != null) && (i < index)) {
+               i++;
+           }
+
+           child.remove();
+       });
+    });
+});
